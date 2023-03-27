04d42f2715ebcfa164ee705d18cafb22bc64fe7c
camicoros <fludbooster@gmail.com>
2023-03-27 13:54:43 +0400

    add readme and dump

--- url_shorter_app/urls.py
+++ url_shorter_app/urls.py
@@ -4,8 +4,8 @@ from .views import RedirectListView, RedirectCreateView, RedirectCounterView, Re
 
 app_name = 'url_shorter_app'
 urlpatterns = [
-    path('', RedirectListView.as_view(), name='index'),
-    path('create/', RedirectCreateView.as_view(), name='create'),
+    path('', RedirectCreateView.as_view(), name='create'),
+    path('list/', RedirectListView.as_view(), name='index'),
     re_path(r'(?P<short_path>[A-Za-z0-9]{6})/detail/', RedirectDetailView.as_view(), name='detail'),
     re_path(r'(?P<short_path>[A-Za-z0-9]{6})/', RedirectCounterView.as_view(), name='redirect'),
 
